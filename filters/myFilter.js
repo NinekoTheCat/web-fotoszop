130b87e4495fecbbf6f2cfb7d4a8f23ad1fc8672
Nineko <45885710+NinekoTheCat@users.noreply.github.com>
2023-03-01 08:11:05 +0000

    attempt nr 1

--- filters/myFilter.js
+++ filters/myFilter.js
@@ -1,4 +1,10 @@
 export function myFilter(imageData) {
-  // 1. implement your filter here
-  // (don't forget to change file name and function name)
+  let imagedata_2 = imageData;
+  for (let y = 0; y < imagedata_2.length; y++) {
+    for (let x = 0; x < imagedata_2[y].length; x++) {
+      imageData[y][x][0] =  imageData[x][y][0]
+      imageData[y][x][1] =  imageData[x][y][1]
+      imageData[y][x][2] =  imageData[x][y][2]
+    }
+  }
 }
